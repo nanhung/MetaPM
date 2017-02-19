114b819d16fa57ec119b4b1a052901ba37915f94
nanhung <nhsieh@cvm.tamu.edu>
2017-02-19 15:41:31 -0600

    Reproduce forest plot

--- Forest.R
+++ Forest.R
@@ -1,26 +1,50 @@
-library(metafor)
+# Load library
+library(meta)
 
-#Construct data frame
-name<-c("Ibfelt et al., 2010", "Ibfelt et al., 2010","Ibfelt et al., 2010","Ibfelt et al., 2010","Ibfelt et al., 2010","Ibfelt et al., 2010","Ibfelt et al., 2010","Ibfelt et al., 2010","Ibfelt et al., 2010","Wilbert et al., 2012","Wilbert et al., 2012", "Costello et al., 2014","Costello et al., 2014","Costello et al., 2014","Costello et al., 2014")
-HR<-c(1.11, 1.43, 1.03, 1.23, 1.41, 1.21, 2.51, 2.79, 1.70, 1.21, 1.23, 1.58, 1.78, 1.48, 1.48)
-LCL<-c(0.65, 0.85, 0.61, 0.73, 0.84, 0.72, 1.15, 1.29, 0.78, 1.11, 1.17, 0.88,1.02, 0.83, 0.77)
-UCL<-c(1.89, 2.41, 1.74, 2.08, 2.36, 2.03, 5.49, 6.04, 3.72, 1.31,1.31,2.63,3.11,2.66,2.85)
-dat<- data.frame(name, HR, LCL, UCL)
+# Load input.tyx
+dat <- read.csv("df.csv", header = TRUE)
 
-#Effect sizes == log HR, Estimate sei, Assign values for plotting
-dat$yi <- with(dat, log(HR))
-dat$sei<- with(dat, (log(UCL)-log(LCL))/(2*1.96))
-labs <- dat$name
+# If RR's and 95% CI's, run these commented out lines
+dat$yi <- with(dat, log(RR))
+dat$sei<- with(dat, (log(ci.ub)-log(ci.lb))/(2*1.96))
 
-# Combine data into summary estimate
-res <- rma(measure="RR", yi, sei, data=dat, method="DL")
-summary(res)
+# Data subsetting
+data1<-subset(dat, endpoint=="incidence")
+data2<-subset(dat, endpoint=="death" & disease=="hd" &exposure=="workplace")
+data3<-subset(dat, endpoint=="death" & disease=="cd" &exposure=="workplace")
+data4<-subset(dat, endpoint=="death" & disease=="lc" &exposure=="workplace")
 
-## A fixed-effects model is fitted by setting method="FE". A randomeffects model is fitted by setting method="ML" (the default)
-## method="DL" = DerSimonian-Laird estimator
+data5<-subset(dat, endpoint=="death" & disease=="all" &exposure=="env")
+data6<-subset(dat, endpoint=="death" & disease=="cvd" &exposure=="env")
+data7<-subset(dat, endpoint=="death" & disease=="hd" &exposure=="env")
 
-forest(res, transf=exp, refline=1, xlab="HR (95%CI)", slab=labs,  showweight=TRUE, mlab="Summary Estimate")
-mtext(paste("I-squared=",summary(res)$I^2),side=3, line=1)
-mtext(paste("Heterogeneity p-value=",summary(res)$QEp),side=3, line=-0.25)
-text(26, 16.5, "HR [95% CI]", pos = 2)
-text(18, 16.5, "Weight", pos = 2)
+# Env All death
+meta1<-metagen(data5$yi, data5$sei, studlab=paste(author,year), sm="RR",data=data5)
+forest(meta1)
+
+# Env Cvd death
+meta2<-metagen(data6$yi, data6$sei, studlab=paste(author,year), sm="RR",data=data6)
+forest(meta2)
+
+# Env hd death
+meta3<-metagen(data7$yi, data7$sei, studlab=paste(author,year), sm="RR",data=data7)
+forest(meta3)
+
+# workplace cvd incidence
+meta4<-metagen(data1$yi, data1$sei, studlab=paste(author,year), sm="RR",data=data1)
+forest(meta4)
+
+# workplace hd death
+meta5<-metagen(data2$yi, data2$sei, studlab=paste(author,year), sm="RR",data=data2)
+forest(meta5)
+
+# workplace cd death
+meta6<-metagen(data3$yi, data3$sei, studlab=paste(author,year), sm="RR",data=data3)
+forest(meta6)
+
+# workplace lc death
+meta7<-metagen(data4$yi, data4$sei, studlab=paste(author,year), sm="RR",data=data4)
+forest(meta7)
+
+
+
